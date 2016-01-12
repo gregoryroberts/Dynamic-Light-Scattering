947912b7937af630557dbd5074c6975ba9e5982d
Greg Roberts <groberts@pa-magic-049s-MacBook-Pro.magicleap.ds>
2016-01-11 23:00:56 -0800

    small formatting change in cmos imager unit test

--- UnitTests/Physics/CMOSImager/Source/CMOSImager.cc
+++ UnitTests/Physics/CMOSImager/Source/CMOSImager.cc
@@ -12,7 +12,7 @@ int main( int argc, char * argv[] )
 	CMOSImager< double, double, 100, 100 > imager( 0.1, 0.1 );
 
 	imager.Shutter();
-	imager.IncidentRay( double(5.32), double(4.12), double(5.0) );
+	imager.IncidentRay( double( 5.32 ), double( 4.12 ), double( 5.0 ) );
 	
 	DLS_ASSERT( imager( 41, 53 ) == 5.0, "Intensity not properly accumulated" );
 
